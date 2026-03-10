e079d35a0e6e03593c36ee3c71da2802aaf6996b
cbcb1728-na <greek.05.19@gmail.com>
2026-03-10 14:15:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+﻿const CACHE_NAME = 'mujineko-honkoku-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.1.0/openseadragon.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
